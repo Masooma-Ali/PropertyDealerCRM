fc1daab661148f057c8e4e6e556fc9dd19dadf1f
Masooma-Ali <masoomaali347@gmail.com>
2026-04-29 18:07:16 +0500

    fixing user.js error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['mongoose'],
+  serverExternalPackages: ['mongoose'],
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    });
+    return config;
   },
 };
 
